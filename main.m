542651c2659b8c73f59f71c00ef95910222a91a0
MB6 <MB6@MB6.local>
2010-12-04 04:28:23 +0100

    index.html

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  test
+//
+//  Created by MB6 on 04/12/10.
+//  Copyright 2010 Kandalu Software Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
